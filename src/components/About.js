b1b9060f586bad2316b56df877625cbb418f6761
emmadal <emmanueldalougou@gmail.com>
2022-04-26 15:01:42 +0000

    add About component

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React, { memo } from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
+
+const About = () => {
+  return (
+    <MDBContainer>
+      <MDBRow className="justify-content-center">
+        <MDBCol className="text-center">
+          <h2 className="section-title mt-4">
+            Pourquoi <span className="has-line"> Askoacademy ?</span>
+          </h2>
+          <h1 className="css-1ktz1md-Alpa text-black">
+            Learn the data skills you need online at your own pace—from
+            non-coding essentials to data science and machine learning.
+          </h1>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default memo(About);
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,17 @@
 import { memo } from "react"
 import Menu from "./Menu"
 import Presentation from "./Presentation"
+import About from "./About";
 import "../styles/custom.css"
 
 const Layout = () => {
-    return(
-        <div>
-            <Menu />
-            <Presentation />
-        </div>
-    )
+    return (
+      <div>
+        <Menu />
+        <Presentation />
+        <About />
+      </div>
+    );
 }
 
 export default memo(Layout)
